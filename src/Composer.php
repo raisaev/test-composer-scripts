3276f800d9e4d3ff3151c248b2003b9d02b090f5
4wjal7rhnqt6s <roman.isaev.a@gmail.com>
2018-05-28 16:43:00 +0300

    init test env

--- src/Composer.php
+++ src/Composer.php
@@ -16,19 +16,16 @@ class Composer
         $installationManager = $event->getComposer()->getInstallationManager();
 
         $originDir = $installationManager->getInstallPath($package) .DIRECTORY_SEPARATOR. self::$cronFilePath;
-
         $targetDir = $event->getComposer()->getConfig()->get('vendor-dir') .'/../'. self::$cronFilePath;
-
 
         //copy(self::$cronFilePath, $targetPath);
 
         var_dump(
             $originDir,
+            $targetDir,
             $package->getName(),
             $installationManager->getInstallPath($package),
-            realpath($installationManager->getInstallPath($package)),
-            $targetDir
-//            $originDir, $targetDir,
+            realpath($installationManager->getInstallPath($package))
         );
         die;
     }
@@ -36,11 +33,45 @@ class Composer
     public static function postUpdateCmd(\Composer\Script\Event $event)
     {
         $event->getIO()->write('postUpdateCmd event fired');
+
+        $package = $event->getComposer()->getPackage();
+        $installationManager = $event->getComposer()->getInstallationManager();
+
+        $originDir = $installationManager->getInstallPath($package) .DIRECTORY_SEPARATOR. self::$cronFilePath;
+        $targetDir = $event->getComposer()->getConfig()->get('vendor-dir') .'/../'. self::$cronFilePath;
+
+        //copy(self::$cronFilePath, $targetPath);
+
+        var_dump(
+            $originDir,
+            $targetDir,
+            $package->getName(),
+            $installationManager->getInstallPath($package),
+            realpath($installationManager->getInstallPath($package))
+        );
+        die;
     }
 
     public static function postInstallCmd(\Composer\Script\Event $event)
     {
         $event->getIO()->write('postInstallCmd event fired');
+
+        $package = $event->getComposer()->getPackage();
+        $installationManager = $event->getComposer()->getInstallationManager();
+
+        $originDir = $installationManager->getInstallPath($package) .DIRECTORY_SEPARATOR. self::$cronFilePath;
+        $targetDir = $event->getComposer()->getConfig()->get('vendor-dir') .'/../'. self::$cronFilePath;
+
+        //copy(self::$cronFilePath, $targetPath);
+
+        var_dump(
+            $originDir,
+            $targetDir,
+            $package->getName(),
+            $installationManager->getInstallPath($package),
+            realpath($installationManager->getInstallPath($package))
+        );
+        die;
     }
 
     //########################################
